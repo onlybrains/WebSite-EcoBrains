4ac911c8aa5197045b5e3931f13cabf3211431cf
Matheus Franciscone <mathfranciscone@gmail.com>
2020-09-29 17:23:24 -0300

    Add accessibility

--- app/Views/layout/index.php
+++ app/Views/layout/index.php
@@ -36,7 +36,19 @@
 </head>
 
 <body>
-
+    <!-- VLibras -->
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>
+    <!-- End VLibras -->
+    
     <?= $this->renderSection('content') ?>
 
     <!-- JavaScript (Opcional) -->
