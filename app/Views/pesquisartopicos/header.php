a4abc06c0895c1a03fba9e71aa925f8ef5bd5449
LuigiRamires <54213955+LuigiRamires@users.noreply.github.com>
2020-09-30 09:34:13 -0300

    Cards of topics are created
    - Created the cards that will show the topics;
    - Need fix the button on right. The margin-top is not in the way that we want to be in the final code.
    - Need to fix the responsivity of the cards that have banners.

--- app/Views/pesquisartopicos/header.php
+++ app/Views/pesquisartopicos/header.php
@@ -4,81 +4,37 @@
       <h1 class="welcome ml-5 mr-3 mt-3 pt-5">Pesquisas</h1>
     </div>
     <div class="col-lg-12 col-md-12">
-      <p class="subtitles-coop ml-5 mr-3 mt-3 pt-2">Pesquisar Tópicos de Negociação:</p>
+      <p class="subtitles-coop ml-5 mr-3 mt-3 pt-2">Resultado da pesquisa:</p>
     </div>
   </div>
-</div>
-<div class="row my-2">
-  <div class="row ml-3">
-    <p class="topic-desc">Data Limite: DD/MM/YYYY </p>
-    <p class="topic-desc">Data Limite: DD/MM/YYYY </p>
-    <p class="topic-desc">Data Limite: DD/MM/YYYY </p>
-  </div>
-</div>
-<!-- XX -->
-<div class="row my-2">
-  <div class="col-lg-12 col-md-12 pb-3">
-    <div class="card">
-      <div class="card-body">
-        <a href="#" class="topic-button p-2 mt-4 float-right"><img src="imgs/topics-vector.png" width="37" height="25"> </a>
-        <div class="row">
-          <h5 class="card-title topic-title ml-3">Nome do tópico</h5>
-        </div>
-        <div class="row ml-3">
-          <p class="topic-desc">Data Limite: DD/MM/YYYY </p>
-        </div>
-        <div class="row ml-3">
-          <p class="topic-desc">Data Limite: DD/MM/YYYY </p>
-        </div>
-        <div class="row ml-3">
-          <p class="topic-desc">Data Limite: DD/MM/YYYY </p>
+  <!-- Topics that the Coop. are offering their services (START) -->
+  <div class="row my-2">
+    <div class="col-lg-12 col-md-12 pb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="container">
+              <a href="#" class="topic-button p-2 float-right" style="margin-top: 66px;"><img src="imgs/topics-vector.png" width="37" height="25"> </a>
+              <img src="imgs/image-random.png" class="mr-5 mt-3 float-right">
+          </div>
+          <div class="row">
+              <h5 class="card-title topic-title ml-3">Nome do tópico</h5>
+              <img src="imgs/selo-ecoB.png" height="43" width="36" class="ml-3 mt-1 mb-1">
+          </div>
+          <div class="row ml-3">
+            <strong class="topic-desc">Aproximadamente 17 Km |</strong>
+            <p class="topic-desc ml-1"> Parque Rizzo - Cotia/SP</p>
+          </div>
+          <div class="row ml-3">
+            <strong class="topic-desc">Data Limite: 30/12/2020</strong>
+            <p class="topic-desc ml-4">Peso: 30 Kg</p>
+          </div>
+          <div class="row ml-3">
+            <p class="topic-desc">Tipo: Papelão</p>
+            <p class="topic-desc ml-4">Reputação: </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-<!-- Topics that the Coop. are offering their services (START) -->
-<div class="row my-2">
-  <div class="col-lg-12 col-md-12 pb-3">
-    <div class="card">
-      <div class="card-body">
-        <a href="#" class="topic-button p-2 mt-4 float-right"><img src="imgs/topics-vector.png" width="37" height="25"> </a>
-        <div class="row">
-          <h5 class="card-title topic-title ml-3">Nome do tópico</h5>
-        </div>
-        <div class="row">
-          <p class="topic-desc ml-3">Data Limite: DD/MM/YYYY </p>
-          <p class="topic-desc ml-3">Material: Material </p>
-          <p class="topic-desc ml-3">Peso: 00Kg</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Topics that the Coop. are offering their services (END) -->
-<hr />
-<div class="row my-2">
-  <div class="col-lg-12 col-md-12">
-    <p class="subtitles-coop ml-5 mr-3 mt-3 pt-2">Tópicos que achamos que combina com você:</p>
-  </div>
-</div>
-<!-- Topics that the Coop. maybe have interest to participate (START) -->
-<div class="row my-2">
-  <div class="col-lg-12 col-md-12 pb-3">
-    <div class="card">
-      <div class="card-body">
-        <a href="#" class="topic-button p-2 mt-4 float-right"><img src="imgs/topics-vector.png" width="37" height="25"> </a>
-        <div class="row">
-          <h5 class="card-title topic-title ml-3">Nome do tópico</h5>
-        </div>
-        <div class="row">
-          <p class="topic-desc ml-3">Data Limite: DD/MM/YYYY </p>
-          <p class="topic-desc ml-3">Material: Material </p>
-          <p class="topic-desc ml-3">Peso: 00Kg</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Topics that the Coop. maybe have interest to participate (END) -->
+  <!-- Topics that the Coop. are offering their services (END) -->
 </div>
