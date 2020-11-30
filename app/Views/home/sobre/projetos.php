5a5ccab6dd78b5d517cbeb850907e5c8cd36767e
Cleyson Silva <cleysonsilva.me@gmail.com>
2020-11-30 19:23:46 -0300

    responsive img projeto ecobrains

--- app/Views/home/sobre/projetos.php
+++ app/Views/home/sobre/projetos.php
@@ -11,15 +11,12 @@
         <div class="tab-content" id="nav-pills-content">
           <div class="tab-pane fade show active" id="nav-item-01" role="tabpanel">
             <div class="row">
-              <div class="col-sm-6">
-                <!-- <div class="col-lg-6 d-none d-lg-block">
-                  <img class="my-1 ml-5 WeAreEcoBrains" src="imgs/OnlyBrains.png" >
-                </div> -->
-                <div class="col-lg-6 d-none d-lg-block">
-                  <img class="my-1 ml-5 OnlyBrainsImage mt-5" src="imgs/imgo.jpg">
+              <div class="col-6">
+                <div class="w-100">
+                  <img class="my-1 img-fluid rounded-lg" src="imgs/ecobrains-logo-completo.jpg">
                 </div>
               </div>
-              <div class="col-lg-6 col-md-12">
+              <div class="col-6">
                 <p class="ecobrainsParagrafhGreen">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis error modi vero aliquam et ab earum sint eligendi velit perferendis. Vitae possimus esse ipsa illum et iste velit non quisquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo voluptatem pariatur doloremque corrupti perferendis iste sint tempore reprehenderit, fugiat obcaecati! Vero molestiae inventore aliquid? Tenetur eveniet error fuga optio sapiente! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur alias quaerat vel accusantium reiciendis modi, nobis laboriosam facere, sit magnam ipsam quis eum! Unde animi delectus dolorem vitae harum explicabo.
                 </p>
