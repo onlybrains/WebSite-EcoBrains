672857f65b93e44507d45e97881dbb1809708ca5
LuigiRamires <54213955+LuigiRamires@users.noreply.github.com>
2020-11-30 19:43:30 -0300

    Update team.php

--- app/Views/home/home/team.php
+++ app/Views/home/home/team.php
@@ -7,7 +7,9 @@
     <!-- Team item -->
     <div class="col-xl-3 col-sm-6 mb-0 cardsCentro">
       <div class="cardsColor rounded shadow-sm py-5 px-4">
-        <img src="https://avatars1.githubusercontent.com/u/62582404?s=460&u=29db25e5cdf416891054e2130bb29b59ec824898&v=4" alt="" width="100" class="cardsCirculo img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+        <img
+          src="https://avatars1.githubusercontent.com/u/62582404?s=460&u=29db25e5cdf416891054e2130bb29b59ec824898&v=4"
+          alt="" width="100" class="cardsCirculo img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
         <h5 class="mb-0">Cleyson Silva</h5>
         <span class="small text-uppercase text-muted">Developer - Co-founder</span>
         <p class="cor_Verde">
@@ -18,13 +20,16 @@
         </p>
         <ul class="social mb-0 list-inline mt-3">
           <li class="list-inline-item">
-            <a href="https://www.instagram.com/cleysonsilva.me/" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.instagram.com/cleysonsilva.me/" class="social-link cardsRedesSociais"
+              target="_blank"><i class="fa fa-instagram"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="https://www.linkedin.com/in/cleyson-silva-639b01188/" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/cleyson-silva-639b01188/" class="social-link cardsRedesSociais"
+              target="_blank"><i class="fa fa-linkedin"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="https://github.com/cleysonsilvame" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-github"></i></a>
+            <a href="https://github.com/cleysonsilvame" class="social-link cardsRedesSociais" target="_blank"><i
+                class="fa fa-github"></i></a>
           </li>
         </ul>
       </div>
@@ -34,7 +39,9 @@
     <!-- Team item -->
     <div class="col-xl-3 col-sm-6 mb-0 cardsCentro">
       <div class="cardsColor rounded shadow-sm py-5 px-4">
-        <img src="https://avatars3.githubusercontent.com/u/54213955?s=460&u=ffda1647819bde18373c6bc3a3f545d234f28e0a&v=4" alt="" width="100" class="cardsCirculo img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+        <img
+          src="https://avatars3.githubusercontent.com/u/54213955?s=460&u=ffda1647819bde18373c6bc3a3f545d234f28e0a&v=4"
+          alt="" width="100" class="cardsCirculo img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
         <h5 class="mb-0">Luigi de Oliveira</h5>
         <span class="small text-uppercase text-muted">Developer - Co-founder</span>
         <p class="cor_Verde">
@@ -44,13 +51,16 @@
         </p>
         <ul class="social mb-0 list-inline mt-3">
           <li class="list-inline-item">
-            <a href="https://www.instagram.com/_ramireszz/" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.instagram.com/_ramireszz/" class="social-link cardsRedesSociais" target="_blank"><i
+                class="fa fa-instagram"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="https://www.linkedin.com/in/luigi-de-oliveira-ab0111197/" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/luigideoliveira/" class="social-link cardsRedesSociais"
+              target="_blank"><i class="fa fa-linkedin"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="https://github.com/LuigiRamires" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-github"></i></a>
+            <a href="https://github.com/LuigiRamires" class="social-link cardsRedesSociais" target="_blank"><i
+                class="fa fa-github"></i></a>
           </li>
         </ul>
       </div>
@@ -60,7 +70,9 @@
     <!-- Team item -->
     <div class="col-xl-3 col-sm-6 mb-0 cardsCentro">
       <div class="cardsColor rounded shadow-sm py-5 px-4">
-        <img src="https://avatars3.githubusercontent.com/u/63426394?s=460&u=1f9937c0f4fea5b1adb4c9b9dc44e99633e215c8&v=4" alt="" width="100" class="cardsCirculo img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+        <img
+          src="https://avatars3.githubusercontent.com/u/63426394?s=460&u=1f9937c0f4fea5b1adb4c9b9dc44e99633e215c8&v=4"
+          alt="" width="100" class="cardsCirculo img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
         <h5 class="mb-0">Matheus Franciscone</h5>
         <span class="small text-uppercase text-muted">Developer - Co-founder</span>
         <p class="cor_Verde">
@@ -70,13 +82,16 @@
         </p>
         <ul class="social mb-0 list-inline mt-3">
           <li class="list-inline-item">
-            <a href="https://www.instagram.com/_franciscone/" class="social-link cardsRedesSociais" target=" _blank"><i class="fa fa-instagram"></i></a>
+            <a href="https://www.instagram.com/_franciscone/" class="social-link cardsRedesSociais" target=" _blank"><i
+                class="fa fa-instagram"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="https://www.linkedin.com/in/matheus-franciscone/" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/matheus-franciscone/" class="social-link cardsRedesSociais"
+              target="_blank"><i class="fa fa-linkedin"></i></a>
           </li>
           <li class="list-inline-item">
-            <a href="https://github.com/franciscone" class="social-link cardsRedesSociais" target="_blank"><i class="fa fa-github"></i></a>
+            <a href="https://github.com/franciscone" class="social-link cardsRedesSociais" target="_blank"><i
+                class="fa fa-github"></i></a>
           </li>
         </ul>
       </div>
