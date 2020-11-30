821f738ce163b98dc2f32f171923ba0f4a0d7b33
Matheus Franciscone <mathfranciscone@gmail.com>
2020-11-30 18:53:38 -0300

    Translate Validations

--- app/Views/home/sobre/projetos.php
+++ app/Views/home/sobre/projetos.php
@@ -12,22 +12,16 @@
           <div class="tab-pane fade show active" id="nav-item-01" role="tabpanel">
             <div class="row">
               <div class="col-sm-6">
+                <!-- <div class="col-lg-6 d-none d-lg-block">
+                  <img class="my-1 ml-5 WeAreEcoBrains" src="imgs/OnlyBrains.png" >
+                </div> -->
                 <div class="col-lg-6 d-none d-lg-block">
-                  <img class="my-1 ml-5 WeAreEcoBrains" src="imgs/WeAreEcoBrains.jpeg">
+                  <img class="my-1 ml-5 OnlyBrainsImage mt-5" src="imgs/imgo.jpg">
                 </div>
               </div>
               <div class="col-lg-6 col-md-12">
                 <p class="ecobrainsParagrafhGreen">
-                  <span class="ecobrainsParagraphBoldGreen">Como?</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis error modi vero aliquam et ab earum sint eligendi velit perferendis. Vitae possimus esse ipsa illum et iste velit non quisquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo voluptatem pariatur doloremque corrupti perferendis iste sint tempore reprehenderit, fugiat obcaecati! Vero molestiae inventore aliquid? Tenetur eveniet error fuga optio sapiente! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur alias quaerat vel accusantium reiciendis modi, nobis laboriosam facere, sit magnam ipsam quis eum! Unde animi delectus dolorem vitae harum explicabo.
-                  <!--<span class="ecobrainsParagraphBoldGreen">Como?</span> Quando nos fizemos essa pergunta, começamos a
-                                        refletir sobre tudo
-                                        tínhamos a nossa volta, tudo que realmente acontecia no descarte de resíduos.
-                                        Foi aí que começamos a
-                                        entender que tudo que conhecíamos sobre resíduos, sua produção e seu processo de
-                                        descarte era muito vago.
-                                        A partir daí que nossa pesquisa começou e foi <a href="nav-item-02"
-                                            class="ecobrainsParagraphBoldGreen">quando</a> a de fato aprendemos mais um pouco sobre ecologia e
-                                        resíduos. -->
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis error modi vero aliquam et ab earum sint eligendi velit perferendis. Vitae possimus esse ipsa illum et iste velit non quisquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo voluptatem pariatur doloremque corrupti perferendis iste sint tempore reprehenderit, fugiat obcaecati! Vero molestiae inventore aliquid? Tenetur eveniet error fuga optio sapiente! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur alias quaerat vel accusantium reiciendis modi, nobis laboriosam facere, sit magnam ipsam quis eum! Unde animi delectus dolorem vitae harum explicabo.
                 </p>
               </div>
             </div>
